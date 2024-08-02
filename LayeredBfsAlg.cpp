fbb2cfc804619f20f89b8a5fde48e259dc603406
justin-schwinn-0 <justinsch1029@gmail.com>
2024-08-01 20:46:54 -0500

    workig on alg

--- LayeredBfsAlg.cpp
+++ LayeredBfsAlg.cpp
@@ -114,7 +114,7 @@ void LayeredBfsAlg::handleLayerCcMsg(Message msg)
         }
         else
         {
-            //Utils::log("root restart BC not done yet");
+            Utils::log("root restart BC not done yet");
             broadcastDown();
         }
     }
